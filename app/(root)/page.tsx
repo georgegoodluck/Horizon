c3e85253a03fc444a184c443d16f59bf3e97a19a
George Fabunmi <otegbeyegeorge1@gmail.com>
2025-12-26 20:47:40 +0100

    feat:create intitial sidebar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Goodluck" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
